672297166790687edf7a11c6b3f4a9e870ffccb3
admin <admin@USCS-Mac185.local>
2019-01-03 17:24:11 -0800

    chnaged the BASE URL

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,4 +1,3 @@
-export const BASE_URL = "http://localhost:3501/";
+//export const BASE_URL = "http://localhost:3501/";
+export const BASE_URL = "https://homerental-api.herokuapp.com/";
 export const CLOUDINARY_URL = "https://res.cloudinary.com/arihant95/image/upload/v1546556005/";
-//export const BASE_URL = "https://ec2-54-183-194-12.us-west-1.compute.amazonaws.com:3501/";
-//export const BASE_URL = "http://54.183.194.12:3501/";
