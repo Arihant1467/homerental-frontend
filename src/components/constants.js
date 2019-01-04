7a506a69efdc8a626e31680026019e64066f40c1
admin <admin@USCS-Mac185.local>
2019-01-03 17:36:40 -0800

    minute changes

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,3 +1,3 @@
 //export const BASE_URL = "http://localhost:3501/";
-export const BASE_URL = process.env.BASE_URL || "http://localhost:3501/";
+export const BASE_URL = "https://homerental-api.herokuapp.com/";
 export const CLOUDINARY_URL = "https://res.cloudinary.com/arihant95/image/upload/v1546556005/";
